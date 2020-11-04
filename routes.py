39c3608bc70fe43cdb47c7c9bd85552f2bc62c07
KalyaniMenon <pkalyanimenon@gmail.com>
2020-11-04 22:24:07 +0530

    Datatable

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify
 from flask_sqlalchemy import SQLAlchemy
 import models
 import os
@@ -9,12 +9,106 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+ os.path.join(basedir, 'des
 db = SQLAlchemy(app)
 db.Model.metadata.reflect(db.engine)
 
+
+
 # two decorators, same function
 @app.route('/')
 @app.route('/index.html')
 def index():
     print("Total number of schools is", models.School.query.count())
-    return render_template('index.html', the_title='Tiger Home Page')
+    return render_template('index.html')
+
+@app.route('/index_get_data')
+def stuff():
+  # Assume data comes from somewhere else
+  data = {
+    "data": [
+      {
+        "Nom Com": "1",
+        "Code Iris": "John Q Public",
+        "Rank of ScoreGlobal": "System Architect",
+        "Nom Iris": "$320,800",
+        "Population": "2011/04/25",
+        "Score Global": "Edinburgh",
+        "Acces Aux_interfaces_numeriques_intercommunalite": "5421",
+        "Access Al_information_intercommunalite": "1",
+        "competences_administative_intercommunalite": "John Q Public",
+        "competence_numeriques_intercommunalite": "System Architect",
+        "global_access_intercommunalite": "$320,800",
+        "global_competence_intercommunalite": "2011/04/25"
+      },
+      {
+        "Nom Com": "1",
+        "Code Iris": "John Q Public",
+        "Rank of ScoreGlobal": "System Architect",
+        "Nom Iris": "$320,800",
+        "Population": "2011/04/25",
+        "Score Global": "Edinburgh",
+        "Acces Aux_interfaces_numeriques_intercommunalite": "5421",
+        "Access Al_information_intercommunalite": "1",
+        "competences_administative_intercommunalite": "John Q Public",
+        "competence_numeriques_intercommunalite": "System Architect",
+        "global_access_intercommunalite": "$320,800",
+        "global_competence_intercommunalite": "2011/04/25"
+      },
+      {
+        "Nom Com": "1",
+        "Code Iris": "John Q Public",
+        "Rank of ScoreGlobal": "System Architect",
+        "Nom Iris": "$320,800",
+        "Population": "2011/04/25",
+        "Score Global": "Edinburgh",
+        "Acces Aux_interfaces_numeriques_intercommunalite": "5421",
+        "Access Al_information_intercommunalite": "1",
+        "competences_administative_intercommunalite": "John Q Public",
+        "competence_numeriques_intercommunalite": "System Architect",
+        "global_access_intercommunalite": "$320,800",
+        "global_competence_intercommunalite": "2011/04/25"
+      },
+      {
+        "Nom Com": "1",
+        "Code Iris": "John Q Public",
+        "Rank of ScoreGlobal": "System Architect",
+        "Nom Iris": "$320,800",
+        "Population": "2011/04/25",
+        "Score Global": "Edinburgh",
+        "Acces Aux_interfaces_numeriques_intercommunalite": "5421",
+        "Access Al_information_intercommunalite": "1",
+        "competences_administative_intercommunalite": "John Q Public",
+        "competence_numeriques_intercommunalite": "System Architect",
+        "global_access_intercommunalite": "$320,800",
+        "global_competence_intercommunalite": "2011/04/25"
+      },
+      {
+        "Nom Com": "1",
+        "Code Iris": "John Q Public",
+        "Rank of ScoreGlobal": "System Architect",
+        "Nom Iris": "$320,800",
+        "Population": "2011/04/25",
+        "Score Global": "Edinburgh",
+        "Acces Aux_interfaces_numeriques_intercommunalite": "5421",
+        "Access Al_information_intercommunalite": "1",
+        "competences_administative_intercommunalite": "John Q Public",
+        "competence_numeriques_intercommunalite": "System Architect",
+        "global_access_intercommunalite": "$320,800",
+        "global_competence_intercommunalite": "2011/04/25"
+      },
+      {
+        "Nom Com": "1",
+        "Code Iris": "John Q Public",
+        "Rank of ScoreGlobal": "System Architect",
+        "Nom Iris": "$320,800",
+        "Population": "2011/04/25",
+        "Score Global": "Edinburgh",
+        "Acces Aux_interfaces_numeriques_intercommunalite": "5421",
+        "Access Al_information_intercommunalite": "1",
+        "competences_administative_intercommunalite": "John Q Public",
+        "competence_numeriques_intercommunalite": "System Architect",
+        "global_access_intercommunalite": "$320,800",
+        "global_competence_intercommunalite": "2011/04/25"
+      }]
+  }
+  return jsonify(data)
 
 @app.route('/symbol.html')
 def symbol():

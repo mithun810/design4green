a187a9daf6ce9525f2b2ee5ec33fc2cfdcf88cc8
KalyaniMenon <pkalyanimenon@gmail.com>
2020-11-05 20:02:14 +0530

    filterChanges

--- static/assets/js/scripts.js
+++ static/assets/js/scripts.js
@@ -139,6 +139,153 @@
         });
     })
 
-   
+    $('#inputDepartment').change(function(){
+        var posturl="/index_get_data";
+        //var posturl = {{ url_for("stuff") }};
+         var val = {
+                     region: $("#inputRegion").val(),
+                     department : $(this).val(),
+                     change : "department",
+                     donnees_infra_communales : $("#inputDonnes").val(),
+                     Choix_de_Point_Reference : $("#inputChoice").val()};
+     
+         $.ajax({
+             type: 'POST',
+             url: posturl, 
+             dataType: 'json',
+             contentType: 'application/json',
+             data: JSON.stringify(val),
+             success: function(response){ 
+                 console.log(response.data); 
+                 $("#inputInterCommunalities").html("");
+                 $(response.data.distinct_filter).each(function () {
+                     $("<option />", {
+                         val: this,
+                         text: this
+                     }).appendTo("#inputInterCommunalities");
+                 });
+                 var optionText = 'All'; 
+                 var optionValue = 'All'; 
+       
+                 $('#inputInterCommunalities').append(new Option(optionText, optionValue)); 
+ 
+ 
+             },
+             error: function(error){console.log(error)}
+         });
+     })
+     
+     $('#inputInterCommunalities').change(function(){
+        var posturl="/index_get_data";
+        //var posturl = {{ url_for("stuff") }};
+         var val = {
+                     region: $("#inputRegion").val(),
+                     department : $("#inputDepartment").val(),
+                     intercommunalities : $(this).val(),
+                     change : "intercommunalities",
+                     donnees_infra_communales : $("#inputDonnes").val(),
+                     Choix_de_Point_Reference : $("#inputChoice").val()};
+     
+         $.ajax({
+             type: 'POST',
+             url: posturl, 
+             dataType: 'json',
+             contentType: 'application/json',
+             data: JSON.stringify(val),
+             success: function(response){ 
+                 console.log(response.data); 
+                 $("#inputCommune").html("");
+                 $(response.data.distinct_filter).each(function () {
+                     $("<option />", {
+                         val: this,
+                         text: this
+                     }).appendTo("#inputCommune");
+                 });
+                 var optionText = 'All'; 
+                 var optionValue = 'All'; 
+       
+                 $('#inputCommune').append(new Option(optionText, optionValue)); 
+ 
+ 
+             },
+             error: function(error){console.log(error)}
+         });
+     })
+
+     $('#inputCommune').change(function(){
+        var posturl="/index_get_data";
+        //var posturl = {{ url_for("stuff") }};
+         var val = {
+                     region: $("#inputRegion").val(),
+                     department : $("#inputDepartment").val(),
+                     intercommunalities : $("#inputInterCommunalities").val(), 
+                     change : "commune",
+                     commune : $(this).val(),
+                     donnees_infra_communales : $("#inputDonnes").val(),
+                     Choix_de_Point_Reference : $("#inputChoice").val()};
+     
+         $.ajax({
+             type: 'POST',
+             url: posturl, 
+             dataType: 'json',
+             contentType: 'application/json',
+             data: JSON.stringify(val),
+             success: function(response){ 
+                 console.log(response.data); 
+             },
+             error: function(error){console.log(error)}
+         });
+     })
+
+     $('#inputChoice').change(function(){
+        var posturl="/index_get_data";
+        //var posturl = {{ url_for("stuff") }};
+         var val = {
+                     region: $("#inputRegion").val(),
+                     department : $("#inputDepartment").val(),
+                     intercommunalities : $("#inputInterCommunalities").val(), 
+                     change : "Choix_de_Point_Reference",
+                     commune : $("#inputCommune").val(),
+                     donnees_infra_communales : $("#inputDonnes").val(),
+                     Choix_de_Point_Reference : $(this).val()};
+     
+         $.ajax({
+             type: 'POST',
+             url: posturl, 
+             dataType: 'json',
+             contentType: 'application/json',
+             data: JSON.stringify(val),
+             success: function(response){ 
+                 console.log(response.data); 
+             },
+             error: function(error){console.log(error)}
+         });
+     })
+
+     $('#inputDonnes').change(function(){
+        var posturl="/index_get_data";
+        //var posturl = {{ url_for("stuff") }};
+         var val = {
+                     region: $("#inputRegion").val(),
+                     department : $("#inputDepartment").val(),
+                     intercommunalities : $("#inputInterCommunalities").val(), 
+                     change : "donnees_infra_communales",
+                     commune : $("#inputCommune").val(),
+                     donnees_infra_communales : $(this).val(),
+                     Choix_de_Point_Reference : $("#inputChoice").val()};
+     
+         $.ajax({
+             type: 'POST',
+             url: posturl, 
+             dataType: 'json',
+             contentType: 'application/json',
+             data: JSON.stringify(val),
+             success: function(response){ 
+                 console.log(response.data); 
+             },
+             error: function(error){console.log(error)}
+         });
+     })
+
 
 })(jQuery);

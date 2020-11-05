82aeff95b345567d6b644a0f5b15022f2a82c564
unknown <mithun.rangaswamy@gmail.com>
2020-11-05 10:12:59 +0530

    post method

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,jsonify
+from flask import Flask, render_template,jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 import models
 import os
@@ -9,7 +9,7 @@ app = Flask(__name__)
 SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious_secret_key')
 # 'mysql://username:password@server/db'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+ os.path.join(basedir, 'designforgreen.db')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Sql@password@localhost:3306/greendesign'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:sqlpassword@localhost:3306/designforgreen'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 db.Model.metadata.reflect(db.engine)
@@ -29,13 +29,15 @@ def index():
   intercomlist=[['Bangalore'], ['Delhi'], ['Chennai']]
   mapbox_access_token = 'pk.eyJ1Ijoidm5pc2hhbnQxMDEyIiwiYSI6ImNraDNoYmIyNzBhZTcycnF5ZTRmamttNmEifQ.0f6AIvvxNgBhNX_zvbhIBw'
   req=get_filters()
-  return render_template('index.html',mapbox_access_token=mapbox_access_token,req=req)
+  data=get_data()
+  return render_template('index.html',mapbox_access_token=mapbox_access_token,req=req,data=data)
 
-@app.route('/index_get_data')
+@app.route('/index_get_data',methods=['POST'])
 def stuff():
   # Assume data comes from somewhere else
+  filters=request.get_json()
   data = {
-    "data": get_data()
+    "data": get_data(filters)
   }
   return jsonify(data)
 
